2dd1fcb6b0bfda7f42a10841d8d5d250c336b969
goodguytim1 <timobrien56@knights.ucf.edu>
2021-03-05 16:13:49 -0500

    Update React App to be more personal

--- timportfolio/src/Work.js
+++ timportfolio/src/Work.js
@@ -17,7 +17,7 @@ function Work() {
                         </div>
                         <h3 className="project-title">Senior Design: Cloud Tracking V2 for Orlando Utilities Commission (OUC) </h3>
                         <p className="project-description"><span>I was the project manager of 8 UCF students who worked on a cloud tracking system that would help the OUC predict solar panel power output.
-                        <br/> <br/> I can't share the live version or the code due to privacy concerns, but please watch my teams short presentation if you would like to know more.</span></p>
+                        <br/> <br/> I can't share the live version or the code due to privacy concerns, but please watch my teams short presentation if you would like to know more and feel free to use the timestamps to navigate.</span></p>
                         <div className="project-actions">
                             <a href="https://youtu.be/lSzXlH8OF40" target="_blank"><button className="project-btn">See Presentation</button></a>
                         </div>
